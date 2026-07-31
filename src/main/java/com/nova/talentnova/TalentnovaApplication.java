df14a1763cbac9f168622963734e5d4ce4fa392b
yire444 <yire2005lol@gmail.com>
2026-07-31 13:10:11 -0500

    pom.xml y properties configurados

---
+++ src/main/java/com/nova/talentnova/TalentnovaApplication.java
@@ -0,0 +1,13 @@
+package com.nova.talentnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nova/talentnova/TalentnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.talentnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
