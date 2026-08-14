896667a2f6052302cec2e1ae93596226fa850279
yire444 <yire2005lol@gmail.com>
2026-08-14 13:09:58 -0500

    modificar el bank service por error de copiar y pegar

--- src/main/java/com/nova/talentnova/service/IBankService.java
+++ src/main/java/com/nova/talentnova/service/IBankService.java
@@ -21,17 +21,4 @@ public interface IBankService {
 
     //ELIMINAR
     void delete(Long id);
-}package com.nova.talentnova.service.impl;
-
-import com.nova.talentnova.dto.BankRequestDTO;
-import com.nova.talentnova.dto.BankResponseDTO;
-import com.nova.talentnova.mapper.BankMapper;
-import com.nova.talentnova.model.Bank;
-import com.nova.talentnova.repository.IBankRepository;
-import com.nova.talentnova.service.IBankService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
+}
